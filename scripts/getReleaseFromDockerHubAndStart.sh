a92d67177d630ef8204a8c4930e1e3cd64a9e890
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-01 16:07:04 -0400

    ajout du stop

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -1,7 +1,6 @@
 #!bin/bash
-set -v
-docker pull $DOCKER_HUB_USERNAME/webapp:release
-#docker stop $(docker ps -q --filter ancestor=$DOCKER_HUB_USERNAME/webapp:release)
-#docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+set –v
+docker $DOCKER_HUB_USERNAME/webapp:release
+docker stop $DOCKER_HUB_USERNAME/webapp:release
+docker rm $DOCKER_HUB_USERNAME/webapp:release
 docker run -itd -p 6521:5000 $DOCKER_HUB_USERNAME/webapp:release
