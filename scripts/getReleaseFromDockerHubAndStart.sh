48a1df55c9e8200a544d2622d20f3005f473da2d
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-01 16:00:54 -0400

    test7

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 set -v
 docker pull $DOCKER_HUB_USERNAME/webapp:release
-docker stop $(docker ps -q --filter ancestor=$DOCKER_HUB_USERNAME/webapp:release)
+#docker stop $(docker ps -q --filter ancestor=$DOCKER_HUB_USERNAME/webapp:release)
 #docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker run -itd -p 6521:5000 $DOCKER_HUB_USERNAME/webapp:release
