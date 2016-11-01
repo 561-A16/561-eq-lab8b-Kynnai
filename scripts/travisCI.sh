e574ed94190252d2b202110ebd21f929906dd296
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-01 16:16:41 -0400

    tests sans variables local

--- scripts/travisCI.sh
+++ scripts/travisCI.sh
@@ -6,10 +6,10 @@ docker run -it --rm -v $PWD/publish:/root/publish -v $PWD/packages:/root/.nuget/
 
 sudo docker build -t webapp:release -f ./dockerfile/release .
 
-docker tag webapp:release $DOCKER_HUB_USERNAME/webapp:release
+docker tag webapp:release kynnai/webapp:release
 docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD
-docker push $DOCKER_HUB_USERNAME/webapp:release
+docker push kynnai/webapp:release
 
-scp -o StrictHostKeyChecking=no ./scripts/getReleaseFromDockerHubAndStart.sh $DOMAINE:~/
-ssh $DOMAINE "chmod +x getReleaseFromDockerHubAndStart.sh"
-ssh $DOMAINE "sh getReleaseFromDockerHubAndStart.sh"
+scp -o StrictHostKeyChecking=no ./scripts/getReleaseFromDockerHubAndStart.sh 159.203.33.172:6521:~/
+ssh 159.203.33.172:6521 "chmod +x getReleaseFromDockerHubAndStart.sh"
+ssh 159.203.33.172:6521 "sh getReleaseFromDockerHubAndStart.sh"
