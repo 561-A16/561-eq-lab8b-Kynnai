a0a1cff314800b16d3b76bfe9df036fb38579e83
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-08 14:25:54 -0500

    build 30

--- scripts/testAndPublish.sh
+++ scripts/testAndPublish.sh
@@ -3,4 +3,5 @@ set -ev
 dotnet restore
 dotnet test ./webapp.unittests
 #rm -rf $PWD/publish/*
-dotnet publish ./webapp --configuration release --output $HOME/publish
+#dotnet publish ./webapp --configuration release --output $HOME/publish
+dotnet publish appweb -o /root/publish
