c696dfef43334b370c8cc6512a6ec71a1b03d110
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-08 14:21:54 -0500

    build29

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set –v
-docker kynnai/webapp:release
-docker stop kynnai/webapp:release
-docker rm kynnai/webapp:release
+docker pull kynnai/webapp:release
+#docker stop kynnai/webapp:release
+#docker rm kynnai/webapp:release
 docker run -itd -p 6521:5000 kynnai/webapp:release
